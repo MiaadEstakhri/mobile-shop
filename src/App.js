8ac22bbfc343038bf4957dfb56147038f11dab0b
Miaad Estakhri <94445130+MiaadEstakhri@users.noreply.github.com>
2022-12-07 15:59:20 +0330

    added AppBar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import AppBar from "./components/AppBar";
 
 function App() {
-  return <div className="bg-slate-400"> پروژه موبایل</div>;
+  return (
+    <div className="bg-gray-100">
+      <AppBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AppBar.jsx
@@ -0,0 +1,18 @@
+import Logo from "../assets/images/logo/Logo.png";
+import { TfiSearch } from "react-icons/tfi";
+
+const AppBar = () => {
+  return (
+    <section className="flex items-center justify-between px-4 pt-11 pb-6">
+      <div className="">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="text-lg font-bold text-slate-800">ساعت هوشمند</div>
+      <div className="w-8 h-8 bg-stone-50 flex items-center justify-center rounded shadow-lg">
+        <TfiSearch className="w-6 h-6 stroke-slate-800" />
+      </div>
+    </section>
+  );
+};
+
+export default AppBar;
